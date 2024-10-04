b45ce9d92f0ad286067803eb07a5bd35b84790b3
bunee <buneeisslo@gmail.com>
2024-10-04 19:33:09 +0530

    Style: made feature section responsive

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -6,77 +6,82 @@ interface Feature {
 }
 
 import {
-  Calculator,
-  BarChart,
-  SpeakerIcon,
-  Fullscreen,
-  Info,
-  ArrowLeftRight,
+  BookOpen,
+  Gauge,
+  ArrowDownNarrowWide,
+  Sparkle,
+  SlidersHorizontal,
+  View,
 } from "lucide-react";
 
-const iconSize = 24;
+const iconSize = 18;
 
 const FeaturesData: Feature[] = [
   {
     id: 1,
-    name: "Calculate YouTube Playlist Length",
-    description: "Determine the total duration of any YouTube playlist.",
-    icon: <Calculator size={iconSize} />,
+    name: "Comprehensive Insights",
+    description:
+      "Get detailed info about your playlists, including total duration, average video duration, number of videos, and specific details for each video.",
+    icon: <BookOpen size={iconSize} />,
   },
   {
     id: 2,
-    name: "Average Duration Insights",
-    description: "Discover the average duration of videos in a playlist.",
-    icon: <BarChart size={iconSize} />,
+    name: "Playback Speed Adjustment",
+    description:
+      "Customize your viewing experience by adjusting the playback speed from 0.25x to 2x, allowing you to plan your binge sessions effectively.",
+    icon: <Gauge size={iconSize} />,
   },
   {
     id: 3,
-    name: "Flexible Playback Speeds",
-    description: "Find out playlist duration at different playback speeds.",
-    icon: <SpeakerIcon size={iconSize} />,
+    name: "Flexible Sorting Options",
+    description:
+      "Easily sort videos by position, duration, views, likes, or publish date by clicking on the column headers in the video table.",
+    icon: <ArrowDownNarrowWide size={iconSize} />,
   },
   {
     id: 4,
-    name: "Smart Sorting Options",
-    description: "Sort playlists by published date, duration, views, and more!",
-    icon: <Fullscreen size={iconSize} />,
+    name: "Efficient Filtering",
+    description:
+      "Use the search bar above the video table to quickly filter and find specific videos within your playlist.",
+    icon: <Sparkle size={iconSize} />,
   },
   {
     id: 5,
-    name: "In-depth Video Details",
-    description: "Explore video stats like duration, views, and likes.",
-    icon: <Info size={iconSize} />,
+    name: "Targeted Analysis",
+    description:
+      "Analyze only the portions of the playlist that interest you with a range selector, enabling focused insights.",
+    icon: <SlidersHorizontal size={iconSize} />,
   },
   {
     id: 6,
-    name: "Calculate Playlist Length Between Specific Videos",
+    name: "Customizable Table View",
     description:
-      'Find playlist length between specific "From" and "To" videos.',
-    icon: <ArrowLeftRight size={iconSize} />,
+      "Tailor your video table by toggeling specific columns using the 'Columns' dropdown, ensuring you see what matters most to you.",
+    icon: <View size={iconSize} />,
   },
 ];
 
 const FeaturesGrid = () => {
   return (
-    <div>
-      <div className="mt-8 grid w-full grid-cols-2 gap-12 md:grid-cols-2 lg:grid-cols-3">
+    <section>
+      <div className="pt-12 grid w-full grid-cols gap-12 md:grid-cols-2 lg:grid-cols-3">
         {FeaturesData.map((feature) => {
           return (
             <div key={feature.id} className="width-fit text-left">
-              <div className="mb-2 w-fit rounded-xl bg-red-500 p-1 text-center text-white ">
+              <div className="mb-2 w-fit rounded-lg bg-red-500 p-1.5 text-center text-white ">
                 {feature.icon}
               </div>
-              <div className="text-lg mb-1 font-bold text-gray-900 dark:text-gray-100">
+              <div className="text-lg mb-1 font-bold md:text-xl text-gray-900 dark:text-gray-100">
                 {feature.name}
               </div>
-              <div className="font-regular max-w-sm text-gray-600  dark:text-gray-400">
+              <div className="font-regular text-sm text-gray-600 md:text-lg dark:text-gray-400">
                 {feature.description}
               </div>
             </div>
           );
         })}
       </div>
-    </div>
+    </section>
   );
 };
 
@@ -84,12 +89,15 @@ const Features = () => {
   return (
     <section className="py-24">
       <div className="my-12 flex w-full flex-col items-center justify-center">
-        <h1 className="mb-2 max-w-3xl text-center text-2xl font-black tracking-tighter text-gray-900 md:text-5xl dark:text-gray-100">
-          Enhance Your YouTube Experience
-        </h1>
-        <p className="max-w-md text-center text-base md:text-xl text-gray-600 dark:text-gray-400">
-          Discover powerful features to manage and analyze your YouTube
-          playlists like never before.
+        <p className="text-xs md:text-sm uppercase tracking-wide text-red-600 mb-2">
+          features
+        </p>
+        <h2 className="mb-2 max-w-4xl text-center text-3xl font-black tracking-tighter text-gray-900 md:text-5xl dark:text-gray-100">
+          Gain Insights into YouTube Playlists
+        </h2>
+        <p className="max-w-xs text-center text-base sm:max-w-sm md:text-xl text-gray-600 dark:text-gray-400">
+          Discover powerful features to manage and analyze any public YouTube
+          playlists.
         </p>
         <FeaturesGrid />
       </div>
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -7,7 +7,7 @@ function Landing() {
   return (
     <>
       <div className="absolute inset-0 -z-10 w-full bg-white bg-[radial-gradient(red_0.1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
-      <main className="container grid place-content-center px-28">
+      <main className="container grid place-content-center md:px-28">
         <Hero />
         <Features />
         <Faq />
